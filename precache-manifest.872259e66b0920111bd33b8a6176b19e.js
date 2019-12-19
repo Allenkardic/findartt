4b550b474e4502df88caea39569c2afdecc2d8cd
Aliakwe <aliakwe@yahoo.com>
2019-12-19 15:15:21 +0100

    Updates

---
+++ precache-manifest.872259e66b0920111bd33b8a6176b19e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "37536d37bc9c41a33fe1d43cf4d9419a",
+    "url": "/arttwork/index.html"
+  },
+  {
+    "revision": "870fc77d6a97a5353eab",
+    "url": "/arttwork/static/css/main.e54ac6df.chunk.css"
+  },
+  {
+    "revision": "486b852306a76c1ac98a",
+    "url": "/arttwork/static/js/2.71af1cdd.chunk.js"
+  },
+  {
+    "revision": "04b21ccc9109def6e71671ed6f2d794a",
+    "url": "/arttwork/static/js/2.71af1cdd.chunk.js.LICENSE"
+  },
+  {
+    "revision": "870fc77d6a97a5353eab",
+    "url": "/arttwork/static/js/main.127164bf.chunk.js"
+  },
+  {
+    "revision": "2aabe60dbb1fc8e8785a",
+    "url": "/arttwork/static/js/runtime-main.ba06c5e8.js"
+  }
+]);
